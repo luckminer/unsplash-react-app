e0041af1c1dc9be8f0814110d804e792f46e0358
luckminer <bloodyblades325@outlook.com>
2019-01-07 10:41:45 -0300

    Use env variables

--- src/App.js
+++ src/App.js
@@ -6,23 +6,26 @@ import Pagination from './components/Pagination'
 
 import './App.css';
 
+const UNSPLASH_BASE_URL = process.env.REACT_APP_UNSPLASH_BASE_URL;
+const UNSPLASH_CLIENT_ID = process.env.REACT_APP_UNSPLASH_CLIENT_ID;
+
 const loadingStatus = {
   success: 'Success',
   error: 'Error',
   loading: 'Loading'
-}
+};
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       photos: [],
       totalPhotos: 0,
       perPage: 9,
       currentPage: 1,
       loadingState: loadingStatus.loading
-    }
-    this.fetchPhotos = this.fetchPhotos.bind(this)
+    };
+    this.fetchPhotos = this.fetchPhotos.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -32,9 +35,7 @@ class App extends Component {
 
   fetchPhotos = async page => {
     const {perPage} = this.state;
-    const baseUrl = 'https://api.unsplash.com/photos/';
-    const clientId = '66abcfa9888252541322564391c295b0e87d52e11997cfd76176e37db4bfccbb';
-    const url = `${baseUrl}?page=${page}&per_page=${perPage}&client_id=${clientId}`
+    const url = `${UNSPLASH_BASE_URL}/photos/?page=${page}&per_page=${perPage}&client_id=${UNSPLASH_CLIENT_ID}`;
     this.setState({loadingState: loadingStatus.loading});
     try {
       const response = await fetch(url);
@@ -50,18 +51,16 @@ class App extends Component {
     } catch (e) {
       this.setState({loadingState: loadingStatus.error})
     }
-  }
+  };
 
   handleSubmit = async query => {
     if (!(!!query && query)) {
       return this.fetchPhotos(1)
     }
 
-    const page = this.state.currentPage
+    const page = this.state.currentPage;
     const {perPage} = this.state;
-    const baseUrl = 'https://api.unsplash.com/search/photos/';
-    const clientId = '66abcfa9888252541322564391c295b0e87d52e11997cfd76176e37db4bfccbb';
-    const url = `${baseUrl}?page=${page}&per_page=${perPage}&query=${query}&client_id=${clientId}`
+    const url = `${UNSPLASH_BASE_URL}/search/photos/?page=${page}&per_page=${perPage}&query=${query}&client_id=${UNSPLASH_CLIENT_ID}`;
     this.setState({loadingState: loadingStatus.loading});
     try {
       const response = await fetch(url);
@@ -77,7 +76,7 @@ class App extends Component {
     } catch (e) {
       this.setState({loadingState: loadingStatus.error})
     }
-  }
+  };
 
   render() {
     return (
